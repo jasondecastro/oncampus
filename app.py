385c40d1c26793a23a2fd065525055fd501ef73a
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-31 14:37:39 -0400

    added public host configuration

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/oncampus', methods=('POST'))
+@app.route('/oncampus', methods=["POST"])
 def oncampus():
   print(request)
 
-app.run()
+app.run(host='0.0.0.0', port=80)
