0c6b5d33af260305159873b595c17b19ba1eb2c2
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-31 15:17:09 -0400

    added a bunch of logic and verification

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+import sqlite3
+conn = sqlite3.connect('oncampus.sqlite')
+c = conn.cursor()
+
 @app.route('/oncampus', methods=["POST"])
 def oncampus():
-  print(request.form['user_name'])
-  return 'testing'
+  if text.lower() == "yes":
+    c.execute("INSERT INTO oncampus VALUES (%s)" % request.form['name'])
+    conn.commit()
+    return "You have checked into the Flatiron School campus."
+  elif text.lower() == "no":
+    if c.execute("SELECT * FROM oncampus WHERE name = %s" % request.form['name']):
+      c.execute("DELETE FROM oncampus WHERE name = %s" % request.form['name'])
+      conn.commit()
+      return "You have checked out of the Flatiron School campus."
+    else:
+      return "You never checked in, but I\'ll check you out anyway."
+  elif text.lower() == "who":
+    return "Coming soon."
+  else:
+    return "Not sure what you are looking for."
 
 app.run(host='0.0.0.0', port=80)
