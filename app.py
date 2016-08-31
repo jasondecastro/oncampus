ada7352c4fcb6685f9ba571516b0e6f8c1f81561
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-31 15:01:51 -0400

    more explicit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/oncampus', methods=["POST"])
 def oncampus():
-  print(request.get)
+  print(request.form['user_name'])
   return 'testing'
 
 app.run(host='0.0.0.0', port=80)
